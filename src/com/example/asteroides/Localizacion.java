01db0efd902e0d940406e8d73426cb2316f9bf58
repson <isaac@repson.org>
2013-02-17 23:12:33 +0100

    Módulo 1 y 2 implementado. Módulo 3: acerda de, salir y menú.

---
+++ src/com/example/asteroides/Localizacion.java
@@ -0,0 +1,60 @@
+package com.example.asteroides;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class Localizacion extends Activity {
+
+	private Button bAcercaDe;
+	private Button bSalir;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		bAcercaDe = (Button) findViewById(R.id.Button03);
+		bAcercaDe.setOnClickListener(new OnClickListener() {
+			public void onClick(View view){
+				lanzarAcercaDe(null);
+			}
+		});
+		
+		bSalir = (Button) findViewById(R.id.Button04);
+		bSalir.setOnClickListener(new OnClickListener() {
+			public void onClick(View view){
+				finish();
+			}
+		});
+	}
+	
+    public void lanzarAcercaDe(View view){
+        Intent i = new Intent(this, AcercaDe.class);
+        startActivity(i);
+    }
+    
+    @Override 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true; /** true -> el menú ya está visible */
+     }
+    
+     @Override 
+     public boolean onOptionsItemSelected(MenuItem item) {
+    	 switch (item.getItemId()) {
+         	case R.id.acercaDe:
+         		lanzarAcercaDe(null);
+                break;
+         }
+         return true; /** true -> consumimos el item, no se propaga*/
+     }
+}
