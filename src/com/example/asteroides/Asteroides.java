d46f3fe4dd9aa59120a1d9ce3048a6b9b6ba4a90
repson <isaac@repson.org>
2013-03-03 22:15:14 +0100

    Added class VistaJuego

--- src/com/example/asteroides/Asteroides.java
+++ src/com/example/asteroides/Asteroides.java
@@ -74,4 +74,9 @@ public class Asteroides extends Activity {
     	 Intent i = new Intent(this, Puntuaciones.class);
     	 startActivity(i);
      }
+     
+     public void lanzarJuego(View view) {
+    	 Intent i = new Intent(this, Juego.class);
+    	 startActivity(i);
+     }
 }
--- src/com/example/asteroides/Juego.java
+++ src/com/example/asteroides/Juego.java
@@ -4,8 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class Juego extends Activity {
-	   @Override public void onCreate(Bundle savedInstanceState) {
-	          super.onCreate(savedInstanceState);
-	          setContentView(R.layout.juego);
+	   @Override
+	   public void onCreate(Bundle savedInstanceState) {
+		   super.onCreate(savedInstanceState);
+	       setContentView(R.layout.juego);
 	   }
-	}
+}
