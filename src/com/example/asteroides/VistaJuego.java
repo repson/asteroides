9b598e0bef21f4574dca8057331f16ca7da594af
repson <isaac@repson.org>
2013-03-03 22:42:08 +0100

    Added ship space to VistaJuego Class

--- src/com/example/asteroides/VistaJuego.java
+++ src/com/example/asteroides/VistaJuego.java
@@ -13,12 +13,21 @@ public class VistaJuego extends View {
     private Vector<Grafico> Asteroides; // Vector con los Asteroides
     private int numAsteroides= 5; // Número inicial de asteroides
     private int numFragmentos= 3; // Fragmentos en que se divide
+    // //// NAVE //////
+    private Grafico nave;// Gráfico de la nave
+    private int giroNave; // Incremento de dirección
+    private float aceleracionNave; // aumento de velocidad
+    // Incremento estándar de giro y aceleración
+    private static final int PASO_GIRO_NAVE = 5;
+    private static final float PASO_ACELERACION_NAVE = 0.5f;
     
     public VistaJuego(Context context, AttributeSet attrs) {
     	super(context, attrs);
         Drawable drawableNave, drawableAsteroide, drawableMisil;
         drawableAsteroide = context.getResources().getDrawable(R.drawable.asteroide1);
         Asteroides = new Vector<Grafico>();
+        drawableNave = context.getResources().getDrawable(R.drawable.nave);
+        nave = new Grafico(this, drawableNave);
         
         for (int i = 0; i < numAsteroides; i++) {
         	Grafico asteroide = new Grafico(this, drawableAsteroide);
@@ -35,9 +44,13 @@ public class VistaJuego extends View {
         super.onSizeChanged(ancho, alto, ancho_anter, alto_anter);
         // Una vez que conocemos nuestro ancho y alto.
         for (Grafico asteroide: Asteroides) {
-            asteroide.setPosX(Math.random()*(ancho-asteroide.getAncho()));
-            asteroide.setPosY(Math.random()*(alto-asteroide.getAlto()));
+        	do{
+        		asteroide.setPosX(Math.random()*(ancho-asteroide.getAncho()));
+        	    asteroide.setPosY(Math.random()*(alto-asteroide.getAlto()));
+        	} while(asteroide.distancia(nave) < (ancho+alto)/5);
         }
+        nave.setPosX(ancho/2);
+        nave.setPosY(alto/2);
     }
 
     @Override
@@ -46,5 +59,6 @@ public class VistaJuego extends View {
         for (Grafico asteroide: Asteroides) {
             asteroide.dibujaGrafico(canvas);
         }
+        nave.dibujaGrafico(canvas);
     }
 }
